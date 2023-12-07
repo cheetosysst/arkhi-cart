f8c73f1d74e04c5dcacd832deab1736b14e9e001
cheetosysst <thect@protonmail.com>
2023-12-07 21:12:54 +0800

    feat: sidebar content

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,6 +1,34 @@
 import { Footer } from "#/components/footer";
 import { Navbar } from "#/components/navbar";
 import type { PropsWithChildren } from "react";
+import {
+	Beef,
+	Bike,
+	Book,
+	Camera,
+	HeartPulse,
+	Monitor,
+	Moon,
+	Pencil,
+	Plug,
+	Shell,
+	Sun,
+	User,
+} from "lucide-react";
+
+const mockData = [
+	{ title: "Computer", icon: <Monitor /> },
+	{ title: "Life", icon: <User /> },
+	{ title: "Fashion", icon: <User /> },
+	{ title: "Health", icon: <HeartPulse /> },
+	{ title: "Sport", icon: <Bike /> },
+	{ title: "Book", icon: <Book /> },
+	{ title: "Eletronic", icon: <Plug /> },
+	{ title: "Photography", icon: <Camera /> },
+	{ title: "Food", icon: <Beef /> },
+	{ title: "Make Up", icon: <Pencil /> },
+	{ title: "Anime", icon: <Shell /> },
+];
 
 export default function Layout({ children }: PropsWithChildren) {
 	return (
@@ -19,13 +47,30 @@ export default function Layout({ children }: PropsWithChildren) {
 					aria-label="close sidebar"
 					className="drawer-overlay"
 				></label>
-				<ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-					<li>
-						<a>Sidebar Item 1</a>
-					</li>
-					<li>
-						<a>Sidebar Item 2</a>
-					</li>
+				<ul className="menu flex min-h-full w-60 flex-col gap-2 bg-neutral p-4 py-8 text-xl text-base-content">
+					<h1 className="prose mb-4 pl-4 text-2xl font-extrabold text-primary">
+						MacHouse
+					</h1>
+					<label className="ml-4 flex cursor-pointer gap-2 text-neutral-content">
+						<Moon />
+						<input
+							type="checkbox"
+							value="dim"
+							className="theme-controller toggle"
+						/>
+						<Sun />
+					</label>
+					{mockData.map((item) => (
+						<li>
+							<a
+								href="#"
+								className="btn btn-neutral flex justify-start gap-2"
+							>
+								{item.icon}
+								{item.title}
+							</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
